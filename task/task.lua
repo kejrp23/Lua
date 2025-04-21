f86285c15616ca79ebeb8fd099aa2c5eb70f3e03
Kejrp23 <j.pittman51@att.net>
2025-04-20 21:30:17 -0400

    updated tasks.lua to a working task manager file

--- task/task.lua
+++ task/task.lua
@@ -1,42 +1,68 @@
+--Task switcher application
+--Created on 4-20-2025
 
+local tasks = {}
 
-person = {
-	name = {'first','last'},
-	height = 00 ,
-	address = { 
-		'street',
-		'city',
-		'state',
-		'zipcode'
-		}
-	}
+function clear_screen()
+	local cmd = string.sub(package.config, 1, 1) == "\\" and "cls" or "clear"
+	os.execute(cmd)
+end
 
-people = {}
+function add_task()
+	io.write("Enter Task Description: ")
+	local desc = io.read()
+	table.insert(tasks, { desc = desc, done = false })
+	clear_screen()
+	print("New task Entered")
+end
 
+function list_tasks()
+	clear_screen()
+	if #tasks == 0 then
+		print("No Tasks Yet.")
+		return
+	end
+	for i, task in ipairs(tasks) do
+		local status = task.done and "[x]" or "[ ]"
+		print(i .. ". " .. status .. " " .. task.desc)
+	end
+end
 
-function create_person(firstname, lastname, street, city, state,zipcode)
-	table.insert(people, {
-			name = {first = firstname, last = lastname}
-			height = 00,
-			address = {
-				street = street,
-				city = city,
-				state = state,
-				zipcode = zipcode
-				}
-			}
-end 
+function complete_task()
+	io.write("Enter task number to mark complete: ")
+	local index = tonumber(io.read())
+	if tasks[index] then
+		tasks[index].done = true
+		clear_screen()
+		print("Task updated") --added
+	else
+		clear_screen()
+		print("Invalid task number.")
+	end
+end
 
+function main_loop()
+	while true do
+		print("\nTask Switcher")
+		print("1. Add Task")
+		print("2. List Tasks")
+		print("3. Mark Task Complete")
+		print("4. Exit")
+		io.write("Choose an option: ")
+		local choice = io.read()
+		if choice == "1" then
+			add_task()
+		elseif choice == "2" then
+			list_tasks()
+		elseif choice == "3" then
+			complete_task()
+		elseif choice == "4" then
+			print("Exiting...")
+			break
+		else
+			print("Invalid option")
+		end
+	end
+end
 
-function print_people(tbl)
-	for name, in pairs(tbl)
-
-
-
-create_person('Jason','Pittman','Main street', 'la la', 'VA', '123456') 
-		
-
-
-
-
-
+main_loop()
